8802905dfd6836168f07996dc5e340cde851c70e
sechs <philippe.buteau@skema.edu>
2021-07-09 12:55:17 +0200

    Sign in working, project completed

--- src/Components/AuthForm/SignIn.js
+++ src/Components/AuthForm/SignIn.js
@@ -1,11 +1,27 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useContext } from "react";
 import "./AuthForm.css";
 import { useSelector, useDispatch } from "react-redux";
 import { AuthContext } from "../../context/AuthContext";
 import { useHistory } from "react-router-dom";
 
 export default function SignIn() {
+  const [error, setError] = useState("");
+  const history = useHistory();
+  const { login } = useContext(AuthContext);
+
   const showModal = useSelector((state) => state);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      await login(inputs.current[0].value, inputs.current[1].value);
+      history.push("loggedHome");
+      closeModal();
+    } catch {
+      setError("Email ou mot de passe incorrect.");
+    }
+  }
 
   const dispatch = useDispatch();
 
@@ -15,24 +31,40 @@ export default function SignIn() {
     });
   };
 
+  const toggleSignUp = () => {
+    dispatch({
+      type: "TOGGLEUP",
+    });
+  };
+
+  const inputs = useRef([]);
+
+  const addInputs = (el) => {
+    if (el && !inputs.current.includes(el)) {
+      inputs.current.push(el);
+    }
+  };
+
   return (
     <div className={showModal.showSignIn ? "global-modal" : "hide-modal"}>
       <div onClick={closeModal} className="overlay"></div>
 
       <div className="container-modal">
-        <form className="form-auth">
+        <form onSubmit={handleSubmit} className="form-auth">
           <h2>CONNEXION</h2>
           <label htmlFor="mail"> Votre mail</label>
-          <input type="email" id="mail" required />
+          <input type="email" ref={addInputs} id="mail" required />
           <label htmlFor="psw"> Votre mot de passe</label>
-          <input type="password" id="psw" required />
-
+          <input type="password" ref={addInputs} id="psw" required />
+          {error}
           <button className="btn-sign">Se connecter</button>
         </form>
         <button onClick={closeModal} className="btn-close">
           X
         </button>
-        <p className="bottom-help-txt">Besoin de créer un compte ? </p>
+        <p onClick={toggleSignUp} className="bottom-help-txt">
+          Besoin de créer un compte ?{" "}
+        </p>
       </div>
     </div>
   );
